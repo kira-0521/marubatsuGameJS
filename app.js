196e9b5c15284441b9b274a0ac38946a6cf96d72
Kira Tanaka <kiratanaka0521@gmail.com>
2021-01-10 15:37:56 +0900

    I styled it for now.

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
 const box = document.querySelectorAll('.box');
+const turn = document.querySelector('#turn');
 const change = document.querySelector('#change');
+const clearBtn = document.querySelector('#clear');
 
-// boxは配列であるためそれぞれにイベントリスナーをかける
-box.forEach(box => {
+
+// マルバツ入力
+box.forEach((box) => {
     box.addEventListener('click', () => {
         const circle = change.classList.toggle('.circle');
-        // changeのクラスがcircleの時○.それ以外で×.
-        if(circle) {
-            box.innerHTML = "○"
-        } else {
-            box.innerHTML = "×"
-        }
+            if(circle) {
+                box.innerHTML = "○";
+                turn.innerHTML = "Turn: ×";
+                // ローカルストレージへ記録を保存
+                localStorage.setItem(box, "○");
+            } else {
+                box.innerHTML = "×";
+                turn.innerHTML = "Turn: ○";
+                localStorage.setItem(box, "×");
+            }
     })
 });
+
+
+// オールクリアーボタン
+clearBtn.addEventListener('click', () => {
+    for(let i = 0; i < box.length; i++) {
+        box[i].innerHTML = "";
+    }
+    // リセット後に○から始まるように
+    change.classList.remove('.circle');
+    turn.innerHTML = "Turn: ◯";
+});
+
+// 勝敗決定ができない
+
+// ターン表示
+
+
